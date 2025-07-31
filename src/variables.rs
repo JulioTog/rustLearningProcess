c2faba8ee3bc09145bf9326267a4085d5a4bed85
JulioTog <julio@tognetti.ar>
2025-07-30 21:28:49 -0300

    Add variables.rs module with basic variable examples

--- src/variables.rs
+++ src/variables.rs
@@ -1,7 +1,10 @@
 pub fn run() {
+
+    // Variables are declared using the let keyword
+    // Variables are immutable by default
     let x = 5;
     println!("x: {}", x);
+    // trying to change the value of x will give an error
+    //x = 6;
+    println!("x is now: {}", x);
 }
-// Uncomment the next line to see what happens
-// x = 6;
-// println!("x is now: {}", x);
