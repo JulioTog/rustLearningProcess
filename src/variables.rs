f289cdd0f83ff50c4281781bff6abcfe7ce3b4ff
JulioTog <julio@tognetti.ar>
2025-07-30 21:31:37 -0300

    update: make a mut variable

--- src/variables.rs
+++ src/variables.rs
@@ -2,9 +2,9 @@ pub fn run() {
 
     // Variables are declared using the let keyword
     // Variables are immutable by default
-    let x = 5;
+    let mut x = 5;
     println!("x: {}", x);
     // trying to change the value of x will give an error
-    //x = 6;
+    x = 6;
     println!("x is now: {}", x);
 }
